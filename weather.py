ef1cd75d364ec4e6ccad13c509acc25b044904c9
Eric Turlak <ericturlak08@gmail.com>
2026-08-19 09:57:52 -0400

    Added city-based lookup

--- weather.py
+++ weather.py
@@ -1,8 +1,19 @@
 from datetime import datetime
 import requests
 
-latitude = "38.897957"
-longitude = "-77.036560"
+city = input("Enter City: ")
+
+coordinatesResponse = requests.get(f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=10&language=en&format=json")
+coordinatesResponse.raise_for_status()
+coordinatesData = coordinatesResponse.json()
+while "results" not in coordinatesData:
+    print(f"No results found for {city}")
+    city = input("Enter City: ")
+    coordinatesResponse = requests.get(f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=10&language=en&format=json")
+    coordinatesResponse.raise_for_status()
+    coordinatesData = coordinatesResponse.json()
+latitude = (coordinatesData["results"][0]["latitude"])
+longitude = (coordinatesData["results"][0]["longitude"])
 
 url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
 response = requests.get(url)
@@ -16,4 +27,4 @@ time = data['hourly']['time']
 
 for index, hour in enumerate(time):
     if hour == currentHour:
-        print(f"Temperature is currently: {temp[index]} degrees F")
+        print(f"Temperature is currently: {temp[index]}°F")
